148924244be4129a4f743279a9f163ebd67fe66b
BizarrIlliada <kazkoviy@icloud.com>
2024-06-20 03:14:49 +0300

    loader for photo content block

--- src/components/shared/PhotoContentBlockComponent.vue
+++ src/components/shared/PhotoContentBlockComponent.vue
@@ -1,28 +1,45 @@
 <template>
-  <div class="photo-content-block df df--jcsb df--ais" :class="{ 'photo-content-block--reversed': reversed }">
-    <div class="photo-content-block__container">
+  <div :key="photo?.url" class="photo-content-block df df--jcsb" :class="{ 'photo-content-block--reversed': reversed }">
+    <div v-if="$slots.default" class="photo-content-block__container df df--col df--ais">
       <slot></slot>
     </div>
 
-    <div class="photo-content-block__photo-container df df--jcc">
-      <img :src="photo.url" :alt="photo.name" loading="lazy" class="photo-content-block__photo">
+    <div class="photo-content-block__photo-container">
+      <img v-if="photo" :src="photo.url" :alt="photo.name" loading="lazy" class="photo-content-block__photo" @load="hideLoader" @error="hideLoader">
+      <MyLoader v-if="!isPhotoLoaded"></MyLoader>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { ref, watch } from 'vue';
+
+  import MyLoader from '../UI/MyLoader.vue';
   import type { IPhoto } from '@/types';
 
   interface Props {
-    photo: IPhoto;
+    photo: IPhoto | null;
     reversed?: boolean;
   }
 
-  defineProps<Props>();
+  const props = defineProps<Props>();
+
+  const isPhotoLoaded = ref(false);
+
+  function hideLoader() {
+    isPhotoLoaded.value = true;
+  }
+
+  watch(() => props.photo?.url, (newUrl, oldUrl) => {
+    if (newUrl !== oldUrl) {
+      isPhotoLoaded.value = false;
+    }
+  });
 </script>
 
 <style scoped lang="scss">
   .photo-content-block {
+    align-items: stretch;
     gap: 20px;
 
     &--reversed {
@@ -31,27 +48,31 @@
 
     @include tablet {
       flex-direction: column;
-      gap: 12px;
+      gap: 24px;
       flex-flow: column-reverse;
       align-items: center;
     }
 
     &__container {
       max-width: 50%;
+      flex-grow: 1;
 
       @include tablet {
+        width: 100%;
         max-width: 100%;
       }
     }
 
-    &__photo {
-      max-width: 100%;
+    &__photo-container {
+      flex-grow: 1;
+      min-height: 150px;
       box-shadow: $app-shadow;
-      object-fit: cover;
+    }
 
-      @include tablet {
-        margin-bottom: 12px;
-      }
+    &__photo {
+      display: block;
+      width: 100%;
+      object-fit: cover;
     }
   }
 </style>
