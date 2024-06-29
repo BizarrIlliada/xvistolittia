621b886e72decbef98f4eed8ba3632fd09d8a230
BizarrIlliada <kazkoviy@icloud.com>
2024-06-29 20:45:12 +0300

    fixed image displaying on prod

--- src/tools/hooks/helpers.ts
+++ src/tools/hooks/helpers.ts
@@ -1,12 +1,3 @@
 export function useHelpers() {
-  function generateUrl(path: string) {
-    const url = new URL(path.replace('@', '/src'), import.meta.url).href
-    console.log(url);
 
-    return url;
-  }
-
-  return {
-    generateUrl,
-  };
 }
